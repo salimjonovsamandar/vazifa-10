03c60fa37218fddcccdf065b3596ac853bd71448
salimjonovsamandar <salimjonovsamandarbek777@gmail.com>
2024-02-10 14:45:41 +0500

    yaxshi vazifa

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import Conkulyator from '../src/components/Conkulyator'
+
+function App() {
+
+  return (
+    <>
+      <Conkulyator />
+    </>
+  )
+}
+
+export default App
